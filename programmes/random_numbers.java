8ccd73a88a0da6c5f4664562e3991c5bcfd7849e
Timon Ludwig <timon-ludwig@gmx.de>
2026-08-10 14:20:54 +0200

    added random numbers programm

---
+++ programmes/random_numbers.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class random_numbers {
+    public static void main(String[] args){
+
+        Random random = new Random();
+
+        int number1;
+        int number2;
+        double number3;
+        boolean isHeads;
+
+        number1 = random.nextInt(1, 100);  // first number is inclusive, last number is exclusive 
+        number2 = random.nextInt(1, 100);
+        number3 = random.nextDouble(1, 2);
+        isHeads = random.nextBoolean();
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        
+        if(isHeads){
+            System.out.println("heads");
+        }
+        else{
+            System.out.println("tails");
+        }
+    }
+}
