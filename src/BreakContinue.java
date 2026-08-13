b28fc0843c825109056806b14dc6b92df8843a77
TimonLudwig1 <timon-ludwig@gmx.de>
2026-08-13 19:34:31 +0200

    added break and continue

---
+++ src/BreakContinue.java
@@ -0,0 +1,17 @@
+public class BreakContinue {
+    public static void main(String[] args){
+
+        // break = break out of a loop (STOP)
+        // continue = skip current iteration of a loop (SKIP)
+
+        for(int i = 0; i < 10; i++){
+
+            if(i == 5){
+                // break;   // stop at 5
+                continue;   // skip 5 
+            }
+            System.out.print(i+ " ");
+        }
+    }
+    
+}
