331d28b42253e0e684e46d737edd7a591971e1bb
TimonLudwig1 <timon-ludwig@gmx.de>
2026-08-14 20:22:15 +0200

    added Variable Scope

--- notes.md
+++
@@ -1,6 +0,0 @@
-Q. What is a data type?:
-
-A. A set of values and a set of operations on those values.
-
-- in java we have to use "decleration statements" to declare the data type of a variable 
-
---
+++ src/VariableScope.java
@@ -0,0 +1,24 @@
+public class VariableScope {
+
+    // a class variable is declared inside of a class, but not inside of a method
+
+    static int x = 3; // CLASS - can be "seen" by methods inside the main method
+
+    public static void main(String[] args) {
+        
+        // variable scope = where a variable can be accessed    - local and class
+
+        //inside a method, if you declare a variable it has what is considered a "local scope"
+
+        int x = 1;  // LOCAL - outside the curly braces, this variable isn't going to be recognized 
+
+        System.out.println(x);  // java uses LOCAL variables first
+
+        doSomething();
+    }
+    static void doSomething(){      // variables with the same name are allowed, as long as they are within methods
+        int x = 2; //LOCAL
+
+        System.out.println(x);
+    }
+}
