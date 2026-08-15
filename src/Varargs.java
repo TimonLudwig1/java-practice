31d906b27e52972cddccb157a5229afadfd8696b
TimonLudwig1 <timon-ludwig@gmx.de>
2026-08-15 20:20:06 +0200

    Varargs in methods

---
+++ src/Varargs.java
@@ -0,0 +1,30 @@
+public class Varargs {
+    public static void main(String[] args) {
+        
+        // varargs = allow a method to accept a varying # of arguments
+        //           makes methods more flexible, no need for overloaded methods 
+        //           java will pack the arguments into an array
+        //           ... (ellipsis)
+        System.out.println(add(1, 2, 3));
+        System.out.println(average(1, 2, 3, 4, 5, 6, 7));
+    }
+    static int add(int... numbers){     // java compiler puts all those arguments into an array when we send them to a method
+        
+        int sum = 0;
+
+        for(int number : numbers){
+            sum += number;
+        }
+        return sum; 
+    }
+
+    static double average(double... numbers){
+
+        double sum = 0;
+        for(double number : numbers){
+            sum += number;
+        }
+        return sum / numbers.length; 
+    }
+
+}
