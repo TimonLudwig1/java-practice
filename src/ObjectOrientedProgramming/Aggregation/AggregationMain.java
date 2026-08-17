7bd851196949a4a7718eded6855edfeb8ae68e5a
TimonLudwig1 <timon-ludwig@gmx.de>
2026-08-17 21:41:26 +0200

    Aggregation

--- src/ObjectOrientedProgramming/Aggregation/AggregationMain.java
+++ src/ObjectOrientedProgramming/Aggregation/AggregationMain.java
@@ -7,7 +7,14 @@ public class AggregationMain {
         //               One object contains another object as part of its structure,
         //               but the contained object/s can exist independently. 
 
-        
+        Book book1 = new Book("The Fellowship of the Ring", 423);
+        Book book2 = new Book("The Two Towers", 352);
+        Book book3 = new Book("The Return of the King", 416);
 
+        Book[] books = {book1, book2, book3}; 
+
+        Library library = new Library("NYC Public Library", 1879, books);
+
+        library.displayInfo();
     }
 }
