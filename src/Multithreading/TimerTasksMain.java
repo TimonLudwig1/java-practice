733ad1e23818b705008babb09528814554aa0edc
TimonLudwig1 <timon-ludwig@gmx.de>
2026-08-19 22:32:09 +0200

    intro to timer and timer task

---
+++ src/Multithreading/TimerTasksMain.java
@@ -0,0 +1,35 @@
+package Multithreading;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerTasksMain {
+    public static void main(String[] args) {
+        
+        // Timer = Class that schedules tasks at specific times or periodically 
+        //         Useful for: sending notifications, scheduled updates, repetitive actions
+
+        // TimerTask = Represents the task that will be executed by the Timer
+        //             You will extend the TimerTask class to define your task 
+        //             Create a subclass of TimerTask and @Override run() 
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask(){
+
+            int count = 3; 
+
+            @Override 
+            public void run(){
+                System.out.println("Hello");
+                // stopping condition
+                count--; 
+                if (count <= 0) {
+                    System.out.println("TASK COMPLETE!");
+                    timer.cancel(); // .cancel(); to stop
+                } 
+          }
+        };
+
+        timer.schedule(task, 0, 1000);   //(task, delay in ms, period: every ...ms)
+    }
+}
