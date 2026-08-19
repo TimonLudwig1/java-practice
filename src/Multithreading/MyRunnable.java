8e631678d0a63e26e40d12d1e67b1ef308fc4d2d
TimonLudwig1 <timon-ludwig@gmx.de>
2026-08-19 23:55:03 +0200

    threading

---
+++ src/Multithreading/MyRunnable.java
@@ -0,0 +1,23 @@
+package Multithreading;
+
+public class MyRunnable implements Runnable {
+    
+    // any code you want to run seperate - place in the run method
+
+    @Override
+    public void run(){
+
+        for(int i = 1; i <= 5; i++){
+            try{Thread.sleep(1000); // when typing "Thread", it refers to the current thread (here: Main)
+            }
+            catch(InterruptedException e){
+                System.out.println("Thread was interrupted");
+            }
+
+            if (i == 10) {
+                System.out.println("Time's up");
+                System.exit(0); //this ends the programm after i seconds (the entire programm, also main)
+            }
+        }
+    }
+}
---
+++ src/Multithreading/ThreadingMain.java
@@ -0,0 +1,36 @@
+package Multithreading;
+
+import java.util.Scanner;
+
+public class ThreadingMain {
+    public static void main(String[] args) {
+        
+        // Threading = Allows a programm to run multiple tasks simultaneously 
+        //             Helps improve performance with time consuming operations
+        //             (File I/O, network communications, or any background tasks)
+
+        // How to create a Thread:
+        // Option 1. Extend the Thread class (simpler)
+        // Option 2. Implement the Runnable interface (better)
+
+        // without threading, the programm runs on the "Main thread"
+
+        Scanner scanner = new Scanner(System.in); 
+
+        MyRunnable myRunnable = new MyRunnable(); 
+        Thread thread = new Thread(myRunnable); 
+
+        // if we want to end our thread when the main thread is done, we need to create a dameon thread
+        thread.setDaemon(true);
+        thread.start();
+
+        System.out.println("You have 5 seconds to enter your name!");
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine(); 
+        System.out.println("Hello " + name);
+
+        
+
+        scanner.close();
+    }
+}
